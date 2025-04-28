22e4f0f23d3528c87712d2277e77105daf40f569
Ben <hourigan@iastate.edu>
2025-04-27 20:57:12 -0500

    Initial setup for yahoo finance api and analysis

---
+++ backend/__init__.py
@@ -0,0 +1,7 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import Lasso
+import pickle
+
+# Load stock dataset
+stock_dataset = pd.read_csv('')
